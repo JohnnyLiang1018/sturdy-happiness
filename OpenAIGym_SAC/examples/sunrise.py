1b93b0994e787f45160a895d5fb9dbd833e31473
Johnny Liang <johnnyliang@Johnnys-MacBook-Pro.local>
2023-03-02 09:02:55 -0800

    config

--- OpenAIGym_SAC/examples/sunrise.py
+++ OpenAIGym_SAC/examples/sunrise.py
@@ -227,5 +227,5 @@ if __name__ == "__main__":
     log_dir = setup_logger_custom(exp_name, variant=variant)
             
     variant['log_dir'] = log_dir
-    ptu.set_gpu_mode(True, True)
+    ptu.set_gpu_mode(True, False)
     experiment(variant)
