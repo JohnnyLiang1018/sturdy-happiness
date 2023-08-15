11253920ee7c73efe0ea8d74b0f4ab465e2d81e8
Johnny Liang <johnnyliang@Johnnys-MacBook-Pro.local>
2023-08-15 15:06:08 -0700

    minor

--- OpenAIGym_SAC/examples/sunrise.py
+++ OpenAIGym_SAC/examples/sunrise.py
@@ -169,7 +169,7 @@ def experiment(variant):
         log_dir=variant['log_dir'],
     )
 
-    replay_buffer_real.load_buffer(100)
+    replay_buffer_real.load_buffer(5)
     
     trainer = NeurIPS20SACEnsembleTrainer(
         env = sphero_env_sim,
